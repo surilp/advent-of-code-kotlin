f840ce643aa679846e0cde7f502026a1090e6d86
Suril Patel <surilsp@gmail.com>
2021-12-01 16:25:26 -0500

    completes day 1 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        if (input.size <= 1) {
+            return 0
+        }
+        var result = 0
+
+        for (idx in 1 until input.size) {
+            if (input[idx].toInt() > input[idx-1].toInt()) {
+                result++
+            }
+        }
+        return result
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val processedInput = input.map { it.toInt() }.windowed(3)
+
+        if (processedInput.size <= 1) {
+            return 0
+        }
+
+        var result = 0
+
+        for (idx in 1 until processedInput.size) {
+            if (processedInput[idx].sum() > processedInput[idx-1].sum()) {
+                result++
+            }
+        }
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
